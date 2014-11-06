65fdc5c41a1c694709c4c47bc9daafdb89c7e84b
Vitali Bokov <iam@vitali.ee>
2014-11-06 16:40:35 +0000

    added: dummy SVG

--- app/views/zones/index.blade.php
+++ app/views/zones/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 	<h1>{{ $title or "Zones"}}</h1>
 
-	<br /> 
+	<br />
 	<div class='row'>
-		@foreach(Zone::all()->take(3) as $zone)
+		@foreach(Zone::where('title', '!=', 'Lab')->get() as $zone)
 			<div class='col-md-4'>
 				<h3>{{ $zone->title }}</h3>
 				@foreach($zone->users() as $user)
@@ -15,6 +15,63 @@
 		@endforeach
 	</div>
 	<br />
+<!-- 	<svg width="400" height="110">
+  		<rect width="100" height="100" style="fill:rgb(255,255,255);stroke-width:3;stroke:rgb(0,0,0)" />
+	</svg>
+	<svg width="400" height="110">
+  		<rect width="100" height="100" style="fill:rgb(255,255,255);stroke-width:3;stroke:rgb(0,0,0)" />
+	</svg>
+	<svg width="400" height="110">
+  		<rect width="100" height="100" style="fill:rgb(255,255,255);stroke-width:3;stroke:rgb(0,0,0)" />
+	</svg> -->
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	version="1.1"
+	class=""
+	pageAlignment="none"
+	x="0px"
+	y="0px"
+	width="100%"
+	height="300px"
+	viewBox="0 0 1000 300"
+	enable-background="new 0 0 1000 300"
+	xml:space="preserve"
+>
+	<rect
+		x="0%"
+		width="250"
+		height="250"
+		style="fill:#F0DD08;stroke-width:3;stroke:#000000"
+	/>
+
+	<rect
+		x="35%"
+		width="250"
+		height="250"
+		style="fill:#56880A;stroke-width:3;stroke:#000000"
+	/>
+
+	<rect
+		x="70%"
+		width="250"
+		height="250"
+		style="fill:#8F4308;stroke-width:3;stroke:#000000"
+	/>
+
+	<circle cx="12.5%" cy="40%" r="40" stroke="#000000" stroke-width="4" fill="#FFFFCC">
+		<animate
+			attributeName="cx"
+			attributeType="XML"
+			from="12.5%"
+			to="47.5%"
+			begin="3s"
+			dur="5s"
+			fill="freeze"
+		/>
+	</circle>
+</svg>
 
 	<div class='row'>
 		<div class='col-md-12'>
@@ -31,7 +88,7 @@
 			  			</tr>
 			  		</thead>
 			  		<tbody>
-			  			@foreach(ZoneSpot::orderBy('id', 'ASC')->skip(6)->take(ZoneSpot::all()->count())->get() as $zone_change)
+			  			@foreach(ZoneSpot::where('job_id', '!=', 'NULL')->orderBy('created_at', 'DESC')->get() as $zone_change)
 				  			<tr>
 				  				<td>{{ $zone_change->spot->user->first_name }}</td>
 				  				<td>{{ $zone_change->zone->title}}</td>
@@ -81,7 +138,7 @@
 				  						<small class='text-muted'>Tracking "{{ $spot->object->jobs->first()->title }}" event</span>
 				  					@else
 				  						<span class='text-danger'>
-				  							<span class='glyphicon glyphicon-exclamation-sign'></span> Unconfigured SPOT 
+				  							<span class='glyphicon glyphicon-exclamation-sign'></span> Unconfigured SPOT
 				  							<a href='{{ url('spots/'.$spot->id.'/edit') }}' class='btn btn-primary btn-sm pull-right'>
 				  								Configure SPOT
 				  								<span class='glyphicon glyphicon-chevron-right'></span>

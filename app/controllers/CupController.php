a61019fc069a6ccb27784610ce7e025339e9997d
Adam Cornforth <adam@apcdesigner.com>
2014-11-21 15:50:06 +0000

    Got rid of auto-data on CupController

--- app/controllers/CupController.php
+++ app/controllers/CupController.php
@@ -3,12 +3,6 @@
 class CupController extends \BaseController {
 
 	public function getPercent() {
-		$percent = Water::orderBy('id', 'DESC')->first()->water_percent;
-		$percent = $percent - 10;
-		Water::create(array('water_percent' => (int)$percent, 'zone_id' => 1, 'job_id' => 1, 'spot_address'  => '0014.4F01.0000.77A7', 'created_at'    => Carbon::now()->toDateTimeString()));
-		if($percent < 10)
-			Water::create(array('water_percent' => 100, 'zone_id' => 1, 'job_id' => 1, 'spot_address'  => '0014.4F01.0000.77A7', 'created_at'    => Carbon::now()->toDateTimeString()));
-
 		return Response::json(array('percent' => Water::orderBy('id', 'DESC')->first()->water_percent));
 	}
 }

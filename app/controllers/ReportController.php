430970f72d874ad863671b1ccb91a146962bad5c
Vitali Bokov <iam@vitali.ee>
2014-11-24 02:31:20 +0000

    debugging reports

--- app/controllers/ReportController.php
+++ app/controllers/ReportController.php
@@ -85,7 +85,7 @@ class ReportController extends \BaseController {
 	{
 		if($spot_address == 0)
 		{
-			echo $data::orderBy('created_at', 'DESC')->take(50)->get();
+			echo $data::orderBy('created_at', 'DESC')->take(1000)->get();
 		}
 		else
 		{
